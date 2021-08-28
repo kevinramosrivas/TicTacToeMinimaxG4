ac38244b0a17d4c64c88af4911adb49a5f1b8daa
Javier <javierali.perez@unmsm.edu.pe>
2021-08-27 21:48:53 -0500

    Clase jugador

---
+++ src/TresEnRayaHumanoHumano/Jugador.java
@@ -0,0 +1,20 @@
+
+package TresEnRayaHumanoHumano;
+
+
+public class Jugador {
+    private String nombre;
+    
+    public Jugador(){
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
